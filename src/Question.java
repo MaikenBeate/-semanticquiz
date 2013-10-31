4c741e135aed1b9b41fff1c033fcc65e608c9391
Maiken Beate Fjellanger <maikenfjellanger@gmail.com>
2013-10-31 16:23:11 +0100

    yo

--- src/Question.java
+++ src/Question.java
@@ -1,51 +1,33 @@
-
 public class Question {
 	
 	private static String questionString;
 	private static String questionAnswer;
 	private static boolean answered;
-	private static ImageQuestion iQ;
-	private static StringQuestionType sQ;
-	
-	public TypeOfQuestion tOQ;
+	private static MovieQuestionType sQ;
+	public static TypeOfQuestion tOQ;
 	private static Container con;
 	
 	public Question()
 	{
 		answered = false;
-		//iQ = ImageQuestion.values()[((int)(Math.random() * ImageQuestion.values().length))];
-		sQ = StringQuestionType.values()[((int)(Math.random() * StringQuestionType.values().length))];
+		//iQ = MovieImageQuestion.values()[((int)(Math.random() * MovieImageQuestion.values().length))];
+		sQ = MovieQuestionType.values()[((int)(Math.random() * MovieQuestionType.values().length))];
+		
 	}
 	public static void GenerateQuestion()
-	{
-		
-		/*switch(con.questionType)
-		{
-		case String:
-			//questionString = "What is the name of the movie directed by " + con.directorName + " and released in " + con.releaseDate;
-			questionAnswer = con.movieName;
-			break;
-		case Image:
-			questionString = "Who directed " + con.movieName;
-			questionAnswer = con.directorName; 
-			break;
-		}*/
+	{		
+		String returnString = First.values()[tOQ.getValue().firstValue()].toString() 
+				+ tOQ.getValue().toString() 
+				+ (tOQ.getValue().secondValue() != 0? Second.values()[tOQ.getValue().secondValue()].toString() : "");
 	}
-	public enum StringQuestionType implements lowerTree{MovieName, Director, Genre, ReleaseYear}
-	public enum ImageQuestion implements lowerTree{}
+	
 	//Enum returns enum returns random enum type of selected enum type
 	public enum TypeOfQuestion{
-		StringQuestionType(sQ){
+		MovieQuestion(sQ){
 			//Overrides return with relevant type and return cast
 			@Override 
-			public StringQuestionType getValue(){
-				return (StringQuestionType)this.value;
-			}
-		},
-		ImageQuestion(iQ){
-			@Override 
-			public ImageQuestion getValue(){
-				return (ImageQuestion)this.value;
+			public MovieQuestionType getValue(){
+				return (MovieQuestionType)this.value;
 			}
 		};
 		protected lowerTree value;
@@ -56,6 +38,44 @@ public class Question {
 	    	return null;
 	    }
 	}
+	/*public enum PrepositionalStatements{Which(0) movie was(0) released in releaseDate, Who(1) directed moviename
+		
+	}*/
+	
+	public enum MovieQuestionType implements lowerTree{
+		movie(){
+			@Override
+			protected void setFirstSecond(){first = 1; second = 1;}
+			}, 
+			directed(){
+				@Override
+				protected void setFirstSecond(){first = 2; second = 0;}
+				}, 
+				≈genre≈(){
+					@Override
+					protected void setFirstSecond(){first = 0; second = 0;}
+					}, 
+					≈released≈in≈(){
+						@Override
+						protected void setFirstSecond(){first = 0; second = 0;}
+						};
+		
+		private static int first;
+		private static int second;
+		MovieQuestionType(){}
+		@Override
+		public int firstValue() {
+			return first;
+		}
+		@Override
+		public int secondValue(){
+			return second;
+		}
+		
+		protected void setFirstSecond(){}
+	}
+	public enum First{≈, ≈which≈, ≈who≈, ≈what≈}
+	public enum Second{≈, ≈was≈, ≈is≈}
 	
 	public String getQuestion()
 	{
@@ -87,4 +107,7 @@ public class Question {
 	}
 }
 //Empty interface to signify that object is lower in enum tree
-interface lowerTree{}
+interface lowerTree{
+	public int firstValue();
+	public int secondValue();
+}
